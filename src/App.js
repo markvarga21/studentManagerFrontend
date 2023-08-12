f320e99088ca85d8b587d67e550aa49a25d2311e
Varga József Márk <vmark2145@gmail.com>
2023-08-12 19:36:19 +0300

    Added static table.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import TableRow from "./components/TableRow";
+import SearchBar from "./components/SearchBar";
+import TableHead from "./components/TableHead";
+
 function App() {
+  const [isFormActive, setIsFormActive] = useState(false);
+  const staticImageUrl =
+    "https://st.depositphotos.com/2309453/4503/i/450/depositphotos_45030333-stock-photo-young-man-concentrating-as-he.jpg";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>User manager frontend.</p>
-      </header>
+    <div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
+        <SearchBar />
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <TableHead />
+          <tbody>
+            <TableRow
+              userImageUrl={staticImageUrl}
+              email={"asd@gmail.com"}
+              status={"online"}
+            />
+            <TableRow
+              userImageUrl={staticImageUrl}
+              email={"asd@gmail.com"}
+              status={"online"}
+            />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/components/TableHead.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TableHead = () => {
+  return (
+    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" className="px-6 py-3">
+          Name
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Position
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Status
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Action
+        </th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHead;
