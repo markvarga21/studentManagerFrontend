5c9c9802c1f7c7c935dd9e79770e3ec1e9db8d57
Varga József Márk <vmark2145@gmail.com>
2023-10-10 16:49:54 +0200

    Redesigned student addition buttons.

---
+++ src/components/CustomButton.js
@@ -0,0 +1,53 @@
+import { Spinner } from "@material-tailwind/react";
+import React from "react";
+
+const CustomButton = ({
+  buttonType,
+  text,
+  isLoading,
+  loadingText,
+  isDisabled,
+  disabledText,
+  handleButtonClick,
+}) => {
+  return (
+    <div>
+      {isDisabled === true ? (
+        <div>
+          <button
+            disabled
+            className=" text-white bg-gray-500 hover:bg-uniGreenLight font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-uniGreenLight dark:hover:bg-uniGreenLight dark:focus:ring-uniGreenLight hover:cursor-not-allowed"
+          >
+            {disabledText}
+          </button>
+        </div>
+      ) : (
+        <div>
+          {isLoading === false ? (
+            <button
+              onClick={handleButtonClick}
+              type={buttonType}
+              className=" text-white bg-uniGreen hover:bg-uniGreenLight focus:ring-4 focus:outline-none focus:ring-uniGreenLight font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-uniGreenLight dark:hover:bg-uniGreenLight dark:focus:ring-uniGreenLight"
+            >
+              {text}
+            </button>
+          ) : (
+            <div>
+              <button
+                disabled
+                className=" text-white bg-gray-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:cursor-not-allowed"
+              >
+                <div className="flex gap-3">
+                  <Spinner className="h-4 w-4" />
+                  {loadingText}
+                </div>
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CustomButton;
--- src/components/UserFormModal.js
+++ src/components/UserFormModal.js
@@ -3,6 +3,7 @@ import SimpleTextInput from "./SimpleTextInput";
 import GenderSelector from "./GenderSelector";
 import ErrorPanel from "./ErrorPanel";
 import axios from "axios";
+import CustomButton from "./CustomButton";
 
 function UserFormModal({
   title,
@@ -125,54 +126,37 @@ function UserFormModal({
                   {idPhoto && !fillingWasSuccessful ? (
                     <div>
                       {isFillingData ? (
-                        <button
-                          disabled
-                          className="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600  dark:focus:ring-blue-800 disabled:opacity-50 disabled:cursor-not-allowed"
-                        >
-                          <svg
-                            aria-hidden="true"
-                            role="status"
-                            className="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-white-600"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                              fill="#1C64F2"
-                            />
-                          </svg>
-                          Filling...
-                        </button>
+                        <CustomButton
+                          isLoading={true}
+                          loadingText={"Filling..."}
+                        />
                       ) : (
-                        <button
-                          onClick={handleFillFormData}
-                          className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        >
-                          Fill data from passport
-                        </button>
+                        <CustomButton
+                          text={"Fill data from passport"}
+                          isLoading={false}
+                          isDisabled={false}
+                          disabledText={""}
+                          loadingText={""}
+                          handleButtonClick={handleFillFormData}
+                        />
                       )}
                     </div>
                   ) : (
                     <div>
                       {fillingWasSuccessful ? (
-                        <button
-                          disabled
-                          className="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600  dark:focus:ring-blue-800 disabled:opacity-50 disabled:cursor-not-allowed"
-                        >
-                          Successfully filled!
-                        </button>
+                        <CustomButton
+                          isLoading={false}
+                          isDisabled={true}
+                          disabledText={"Successfully filled!"}
+                          loadingText={""}
+                        />
                       ) : (
-                        <button
-                          disabled
-                          className="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600  dark:focus:ring-blue-800 disabled:opacity-50 disabled:cursor-not-allowed"
-                        >
-                          Fill data from passport
-                        </button>
+                        <CustomButton
+                          isLoading={false}
+                          isDisabled={true}
+                          disabledText={"Fill data from passport"}
+                          loadingText={""}
+                        />
                       )}
                     </div>
                   )}
@@ -228,61 +212,44 @@ function UserFormModal({
             </div>
             <div className="flex gap-x-2 flex-row items-center">
               {isSaving ? (
-                <button
-                  disabled
-                  type="button"
-                  className="w-full py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
-                >
-                  <svg
-                    aria-hidden="true"
-                    role="status"
-                    className="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="#1C64F2"
-                    />
-                  </svg>
-                  Saving...
-                </button>
+                <CustomButton
+                  buttonType={"button"}
+                  text={"Save"}
+                  isLoading={true}
+                  isDisabled={true}
+                  disabledText={""}
+                  loadingText={"Saving..."}
+                />
               ) : (
-                <button
-                  type="submit"
-                  className="w-15 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Save
-                </button>
+                <CustomButton
+                  buttonType={"submit"}
+                  text={"Save"}
+                  isLoading={false}
+                  isDisabled={false}
+                  disabledText={""}
+                  loadingText={""}
+                />
               )}
-              <button
-                className="w-15 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                onClick={closeModal}
-              >
-                Close without saving
-              </button>
+
               {idPhoto && !passportIsValid ? (
-                <button
-                  className="w-15 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  onClick={validatePassport}
-                >
-                  Validate passport
-                </button>
+                <CustomButton
+                  buttonType={"button"}
+                  text={"Validate passport"}
+                  isLoading={false}
+                  isDisabled={false}
+                  disabledText={""}
+                  loadingText={""}
+                  handleButtonClick={validatePassport}
+                />
               ) : (
                 <div>
                   {passportIsValid ? (
-                    <button
-                      disabled
-                      type="button"
-                      class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-green-900 focus:outline-none bg-white rounded-lg border border-gray-20focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-green-700 dark:bg-green-800 dark:text-green-400 dark:border-green-600"
-                    >
-                      Passport is valid!
-                    </button>
+                    <CustomButton
+                      isLoading={false}
+                      isDisabled={true}
+                      disabledText={"Passport is valid!"}
+                      loadingText={""}
+                    />
                   ) : (
                     <div>
                       <input
@@ -293,7 +260,7 @@ function UserFormModal({
                       />
                       <label
                         for="idPhoto"
-                        className="w-15 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+                        className="text-white bg-uniGreen hover:bg-uniGreenLight focus:ring-4 focus:outline-none focus:ring-uniGreenLight font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-uniGreenLight dark:hover:bg-uniGreenLight dark:focus:ring-uniGreenLight hover:cursor-pointer"
                       >
                         Upload passport
                       </label>
@@ -302,22 +269,23 @@ function UserFormModal({
                 </div>
               )}
               {selfiePhoto && !selfieIsValid ? (
-                <button
-                  onClick={handleSelfieValidation}
-                  className="w-15 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Validate portrait
-                </button>
+                <CustomButton
+                  text={"Validate portrait"}
+                  isLoading={false}
+                  isDisabled={false}
+                  disabledText={""}
+                  loadingText={""}
+                  handleButtonClick={handleSelfieValidation}
+                />
               ) : (
                 <div>
                   {selfieIsValid ? (
-                    <button
-                      disabled
-                      type="button"
-                      class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-green-900 focus:outline-none bg-white rounded-lg border border-gray-20focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-green-700 dark:bg-green-800 dark:text-green-400 dark:border-green-600"
-                    >
-                      Selfie is valid!
-                    </button>
+                    <CustomButton
+                      isLoading={false}
+                      isDisabled={true}
+                      disabledText={"Selfie is valid!"}
+                      loadingText={""}
+                    />
                   ) : (
                     <div>
                       <input
@@ -328,7 +296,7 @@ function UserFormModal({
                       />
                       <label
                         for="selfiePhoto"
-                        className="w-15 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+                        className="text-white bg-uniGreen hover:bg-uniGreenLight focus:ring-4 focus:outline-none focus:ring-uniGreenLight font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-uniGreenLight dark:hover:bg-uniGreenLight dark:focus:ring-uniGreenLight hover:cursor-pointer"
                       >
                         Upload portrait
                       </label>
