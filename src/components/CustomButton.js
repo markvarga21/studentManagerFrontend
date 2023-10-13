078141ef73a047993dfd038a91503770cecb34ec
Varga József Márk <vmark2145@gmail.com>
2023-10-13 14:52:58 +0200

    Reimplemented CustomButton.

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,5 +1,5 @@
 import { Spinner } from "@material-tailwind/react";
-import React from "react";
+import React, { useEffect } from "react";
 
 const CustomButton = ({
   buttonType,
@@ -10,42 +10,36 @@ const CustomButton = ({
   disabledText,
   handleButtonClick,
 }) => {
+  const disabledStyle =
+    "h-full text-white bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:cursor-not-allowed";
+  const normalStyle =
+    "h-full text-white bg-uniGreen hover:bg-darkUniGreen px-5 py-2.5 font-medium rounded-lg text-sm text-center focus:ring-4 ring-lightUniGreen";
+
+  const [buttonStyle, setButtonStyle] = React.useState(normalStyle);
+
+  useEffect(() => {
+    if (isDisabled === true) {
+      setButtonStyle(disabledStyle);
+    } else {
+      setButtonStyle(normalStyle);
+    }
+  }, [isDisabled]);
   return (
     <div>
-      {isDisabled === true ? (
-        <div>
-          <button
-            disabled
-            className="text-white bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:cursor-not-allowed"
-          >
-            {disabledText}
-          </button>
-        </div>
-      ) : (
-        <div>
-          {isLoading === false ? (
-            <button
-              onClick={handleButtonClick}
-              type={buttonType}
-              className=" text-white bg-uniGreen hover:bg-darkUniGreen px-5 py-2.5 font-medium rounded-lg text-sm text-center focus:ring-4 ring-lightUniGreen"
-            >
-              {text}
-            </button>
-          ) : (
-            <div>
-              <button
-                disabled
-                className="text-white bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:cursor-not-allowed"
-              >
-                <div className="flex gap-3">
-                  <Spinner className="h-4 w-4" />
-                  {loadingText}
-                </div>
-              </button>
-            </div>
-          )}
-        </div>
-      )}
+      <button
+        type={buttonType}
+        className={buttonStyle}
+        onClick={handleButtonClick}
+      >
+        {isLoading === true ? (
+          <div className="flex gap-3">
+            <Spinner className="h-4 w-4" />
+            <p>{loadingText}</p>
+          </div>
+        ) : (
+          <p>{isDisabled === true ? disabledText : text}</p>
+        )}
+      </button>
     </div>
   );
 };
