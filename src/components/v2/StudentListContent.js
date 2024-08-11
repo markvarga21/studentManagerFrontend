6f54a44085c46b5af3ef658c0cb3c1d57dd00574
Varga József Márk <vmark2145@gmail.com>
2024-08-11 10:54:19 +0300

    Refactor export utils.

--- src/components/v2/StudentListContent.js
+++ src/components/v2/StudentListContent.js
@@ -8,7 +8,9 @@ import Pagination from "../icons/Pagination";
 import axios from "axios";
 import ExportIcon from "../icons/ImportIcon";
 import UserModalV2 from "./UserModalV2";
-import { exportJson, exportXml } from "./exportUtils";
+import axios from "axios";
+import toast from "react-hot-toast";
+import formatXml from "xml-formatter";
 
 const StudentListContent = ({
   colorModeColors,
@@ -54,6 +56,107 @@ const StudentListContent = ({
   };
   const [deleted, setDeleted] = useState(1);
   const [isAddStudentActive, setIsAddStudentActive] = useState(false);
+
+  const exportJson = (data, token, API_URL) => {
+    const json = JSON.stringify(data, null, 2);
+    axios
+      .post(`${API_URL}/export/validate/json`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        const valid = res.data;
+        if (valid) {
+          const blob = new Blob([json], { type: "application/json" });
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "students.json";
+          a.click();
+          URL.revokeObjectURL(url);
+        } else {
+          toast.error("Invalid data. Please correct the errors.");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const exportXml = (data, token, API_URL) => {
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>`;
+    const content = data.map((student) => makeStudentXml(student)).join("");
+    const body = xml + `<students>${content}</students>`;
+    const formatted = formatXml(body, {
+      indentation: "  ",
+      collapseContent: true,
+    });
+    axios
+      .post(`${API_URL}/export/validate/xml`, formatted, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/xml",
+        },
+      })
+      .then((res) => {
+        const valid = res.data;
+        if (valid) {
+          const blob = new Blob([formatted], { type: "application/xml" });
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "students.xml";
+          a.click();
+          URL.revokeObjectURL(url);
+        } else {
+          toast.error("Invalid data. Please correct the errors.");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const makeStudentXml = (student) => {
+    const dateOfBirth = student.birthDate.split("-");
+    const issueDate = student.passportDateOfIssue.split("-");
+    const expiryDate = student.passportDateOfExpiry.split("-");
+    const validity = student.validity ? "valid" : "invalid";
+    return `<student id="${student.id}">
+          <name>
+              <firstName>${capitalize(student.firstName)}</firstName>
+              <lastName>${capitalize(student.lastName)}</lastName>
+          </name>
+          <dateOfBirth>
+              <year>${dateOfBirth[0]}</year>
+              <month>${dateOfBirth[1]}</month>
+              <day>${dateOfBirth[2]}</day>
+          </dateOfBirth>
+          <placeOfBirth>${student.placeOfBirth}</placeOfBirth>
+          <countryOfCitizenship>${capitalize(
+            student.countryOfCitizenship
+          )}</countryOfCitizenship>
+          <gender>${String(student.gender).toLowerCase()}</gender>
+          <passportNumber>${student.passportNumber}</passportNumber>
+          <dateOfIssue>
+              <year>${issueDate[0]}</year>
+              <month>${issueDate[1]}</month>
+              <day>${issueDate[2]}</day>
+          </dateOfIssue>
+          <dateOfExpiry>
+              <year>${expiryDate[0]}</year>
+              <month>${expiryDate[1]}</month>
+              <day>${expiryDate[2]}</day>
+          </dateOfExpiry>
+          <status>${validity}</status>
+      </student>  `;
+  };
+
+  const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
   useEffect(() => {
     axios
